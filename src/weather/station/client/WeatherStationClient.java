e0c2436a7e5c9046775117f776a861c5aaf617fe
Petar Peychev <petarpeychev98@gmail.com>
2020-03-12 06:14:22 +0000

    Added skeleton networking logic to connect to the server

--- src/weather/station/client/WeatherStationClient.java
+++ src/weather/station/client/WeatherStationClient.java
@@ -1,15 +1,40 @@
 package weather.station.client;
 
+import java.io.IOException;
+import static java.lang.Integer.parseInt;
+import java.net.Socket;
+
 /**
  * Continuously sends weather data to the server. 
  */
 public class WeatherStationClient {
 
     /**
-     * @param args the command line arguments
+     * Entry point for the weather station client program.
+     * 
+     * Parses an IP address and port from the command line arguments and
+     * runs the client.
+     * 
+     * @param args
+     * @throws IOException 
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        String IP = args[0];
+        int port = parseInt(args[1]);
+        WeatherStationClient client = new WeatherStationClient();
+        client.run(IP, port);
+    }
+
+    /**
+     * Connects to the server at the specified IP address and port.
+     * 
+     * @param IP
+     * @param port
+     * @throws IOException 
+     */
+    private void run(String IP, int port) throws IOException {
+        Socket server = new Socket(IP, port);
+        System.out.println("Connected to server at " + server.getInetAddress());
     }
     
 }
