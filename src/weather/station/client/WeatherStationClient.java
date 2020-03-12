c041214c70951bedd31ef19215fb9dcf47685ea0
Petar Peychev <petarpeychev98@gmail.com>
2020-03-12 04:08:24 +0000

    Added initial netbeans ant project

---
+++ src/weather/station/client/WeatherStationClient.java
@@ -0,0 +1,15 @@
+package weather.station.client;
+
+/**
+ * Continuously sends weather data to the server. 
+ */
+public class WeatherStationClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
