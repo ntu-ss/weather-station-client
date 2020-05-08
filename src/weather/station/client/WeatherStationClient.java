8680c5b4d2af6ded543a3d9f3d869da92e2632dd
Petar Peychev <petarpeychev98@gmail.com>
2020-05-08 08:00:49 +0100

    version 1.0

--- src/weather/station/client/WeatherStationClient.java
+++ src/weather/station/client/WeatherStationClient.java
@@ -1,40 +1,76 @@
 package weather.station.client;
 
 import java.io.IOException;
-import static java.lang.Integer.parseInt;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+import shared.utilities.ClientAuthenticationResponse;
+import shared.utilities.StationDataMessage;
 
-/**
- * Continuously sends weather data to the server. 
- */
+import shared.utilities.StationRegistrationRequest;
+
 public class WeatherStationClient {
+    
+    private final String id;
+    private final String position;
+    private final Socket server;
+    private final ObjectOutputStream objectOutputStream;
+    private final ObjectInputStream objectInputStream;
 
-    /**
-     * Entry point for the weather station client program.
-     * 
-     * Parses an IP address and port from the command line arguments and
-     * runs the client.
-     * 
-     * @param args
-     * @throws IOException 
-     */
-    public static void main(String[] args) throws IOException {
-        String IP = args[0];
-        int port = parseInt(args[1]);
-        WeatherStationClient client = new WeatherStationClient();
-        client.run(IP, port);
+    public WeatherStationClient(String IP, int port, String id, String position) throws IOException {
+        this.id = id;
+        this.position = position;
+        server = new Socket(IP, port);
+        OutputStream outputStream = server.getOutputStream();
+        objectOutputStream = new ObjectOutputStream(outputStream);
+        InputStream inputStream = server.getInputStream();
+        objectInputStream = new ObjectInputStream(inputStream);
+        System.out.println("Connected to server at " + server.getInetAddress());
     }
 
-    /**
-     * Connects to the server at the specified IP address and port.
-     * 
-     * @param IP
-     * @param port
-     * @throws IOException 
-     */
-    private void run(String IP, int port) throws IOException {
-        Socket server = new Socket(IP, port);
-        System.out.println("Connected to server at " + server.getInetAddress());
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        WeatherStationClient client = new WeatherStationClient("127.0.0.1", 8080, 
+                "ID" + (int)(Math.random() * 1000 + 1), 
+                (Math.random() * 10) + ":" + (Math.random() * 10));
+        client.registerWithServer();
+        client.beginSendingData();
+    }
+
+    private void sendObject(Object object) throws IOException {
+        objectOutputStream.writeObject(object);
+        objectOutputStream.flush();
+    }
+
+    private Object readObject() throws IOException, ClassNotFoundException {
+        Object object = objectInputStream.readObject();
+        return object;
+    }
+
+    private void registerWithServer() throws IOException, ClassNotFoundException {
+        sendObject(new StationRegistrationRequest(id));
+        ClientAuthenticationResponse message = (ClientAuthenticationResponse)readObject();
+        if(message.isAccepted()) {
+            System.out.println("Successfully authenticated!");
+        }
+        else {
+            System.out.println("Authentication request denied!");
+        }
+    }
+
+    private void beginSendingData() throws IOException {
+        while(true) {
+            int humidity = (int)(Math.random() * 20 + 10);
+            int temperature = (int)(Math.random() * 20 + 10);
+            sendObject(new StationDataMessage(position, temperature, humidity));
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
     
 }
